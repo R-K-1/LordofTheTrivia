bb55247a125fe7fc31dc52a2a1a6ff8c0333d256
R-K-1 <raymondkalonji@gmail.com>
2017-05-14 22:21:53 -0400

    feat: initial cardview and gridmanager code

--- app/src/main/java/com/example/rkalonji/lordofthetrivia/ExampleFragment.java
+++ app/src/main/java/com/example/rkalonji/lordofthetrivia/ExampleFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +17,35 @@ import android.view.ViewGroup;
  */
 
 public class ExampleFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataset = {"string 1", "string 2", "string 3", "string 4"};
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mRecyclerView = (RecyclerView) getView().findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.example_fragment, container, false);
     }
