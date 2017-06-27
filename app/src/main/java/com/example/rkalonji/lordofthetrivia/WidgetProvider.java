f178e93a74f823072b2f0fb38af110aabb5e3afe
R-K-1 <raymondkalonji@gmail.com>
2017-06-27 13:21:53 -0400

    feat: widget.

---
+++ app/src/main/java/com/example/rkalonji/lordofthetrivia/WidgetProvider.java
@@ -0,0 +1,33 @@
+package com.example.rkalonji.lordofthetrivia;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import java.util.Random;
+
+/**
+ * Created by rkalonji on 06/27/2017.
+ */
+
+public class WidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int count = appWidgetIds.length;
+
+        for (int i = 0; i < count; i++) {
+            int widgetId = appWidgetIds[i];
+            
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.widget_lottr);
+            Intent intent = new Intent(context, WidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        }
+    }
+}
