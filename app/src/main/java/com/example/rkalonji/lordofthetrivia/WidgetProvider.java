ff611c7aadd52915f1bf417e787f40c4db2e2a01
R-K-1 <raymondkalonji@gmail.com>
2017-06-27 16:37:41 -0400

    fix: instantiating right class when launching from widget.

--- app/src/main/java/com/example/rkalonji/lordofthetrivia/WidgetProvider.java
+++ app/src/main/java/com/example/rkalonji/lordofthetrivia/WidgetProvider.java
@@ -25,36 +25,23 @@ public class WidgetProvider extends AppWidgetProvider {
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_lottr);
 
         Intent active = new Intent(context, MainActivity.class);
+        active.putExtra("target", ACTION_LAUNCH_CATEGORIES_FRAGMENT);
         active.setAction(ACTION_LAUNCH_CATEGORIES_FRAGMENT);
-        PendingIntent actionPendingIntent = PendingIntent.getBroadcast(context, 0, active, 0);
+        PendingIntent actionPendingIntent = PendingIntent.getActivity(context, 0, active, 0);
         remoteViews.setOnClickPendingIntent(R.id.widget_launch_categories_grid, actionPendingIntent);
 
-        active = new Intent(context, WidgetProvider.class);
+        active = new Intent(context, MainActivity.class);
+        active.putExtra("target", ACTION_LAUNCH_SETS_FRAGMENT);
         active.setAction(ACTION_LAUNCH_SETS_FRAGMENT);
-        actionPendingIntent = PendingIntent.getBroadcast(context, 0, active, 0);
+        actionPendingIntent = PendingIntent.getActivity(context, 0, active, 0);
         remoteViews.setOnClickPendingIntent(R.id.widget_launch_all_sets_grid, actionPendingIntent);
 
-        active = new Intent(context, WidgetProvider.class);
+        active = new Intent(context, MainActivity.class);
+        active.putExtra("target", ACTION_SCORES_FRAGMENT);
         active.setAction(ACTION_SCORES_FRAGMENT);
-        actionPendingIntent = PendingIntent.getBroadcast(context, 0, active, 0);
+        actionPendingIntent = PendingIntent.getActivity(context, 0, active, 0);
         remoteViews.setOnClickPendingIntent(R.id.widget_launch_scores_grid, actionPendingIntent);
 
         appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
-    }
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(ACTION_LAUNCH_CATEGORIES_FRAGMENT)) {
-            intent.putExtra("target", ACTION_LAUNCH_CATEGORIES_FRAGMENT);
-            context.startActivity(intent);
-        } else if (intent.getAction().equals(ACTION_LAUNCH_SETS_FRAGMENT)) {
-            intent.putExtra("target", ACTION_LAUNCH_SETS_FRAGMENT);
-            context.startActivity(intent);
-        } else if (intent.getAction().equals(ACTION_SCORES_FRAGMENT)) {
-            intent.putExtra("target", ACTION_SCORES_FRAGMENT);
-            context.startActivity(intent);
-        } else {
-            super.onReceive(context, intent);
-        }
     }
 }
